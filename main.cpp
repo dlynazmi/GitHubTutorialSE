3e27ede01ce381f30125c18687fb38d00560df14
Nurazlin Zainal Azmi <nurazlin.azmi@firstcity.edu.my>
2017-10-11 09:37:09 +0800

    Done with the main branch

--- main.cpp
+++ main.cpp
@@ -4,5 +4,7 @@ using namespace std;
 int main(){
 	cout << "Helo world\n";
 	cout << "Working with new branch\n";
+	cout << "Continue working with the main branch\n";
+	cout << endl << endl;
 	return 0;
 }
